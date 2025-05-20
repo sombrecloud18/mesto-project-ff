47e8a172a4a266d6c735d4b2ba1fe2e7e4c11f20
Kate Bilida <sombrecloud@yandex.ru>
2025-05-20 20:38:54 +0300

    Готовый проект

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,17 @@
-// @todo: Темплейт карточки
+function createCard(cardData, deleteCard) {
+    const template = document.querySelector('#card-template').content;
+    const card = template.cloneNode(true).querySelector('.card');
 
-// @todo: DOM узлы
+    card.querySelector('.card__image').src = cardData.link;
+    card.querySelector('.card__image').alt = cardData.name;
+    card.querySelector('.card__title').textContent = cardData.name;
+    card.querySelector('.card__delete-button').addEventListener('click', () => deleteCard(card));
+    return card;
+}
 
-// @todo: Функция создания карточки
+function deleteCard(cardElement) {
+    cardElement.remove();
+}
 
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
+const container = document.querySelector('.places__list');
+initialCards.forEach(data => container.append(createCard(data, deleteCard)));

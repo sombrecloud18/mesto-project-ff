1751fda38a3fef6087de9d9a8914dd40fc191627
Kate <billykatessss@gmail.com>
2025-06-26 15:36:53 +0300

    feat: cards from other users are shown, user can edit his profile and add new cards

--- src/index.js
+++ src/index.js
@@ -28,6 +28,7 @@ const nameInput = editForm.querySelector('.popup__input_type_name');
 const jobInput = editForm.querySelector('.popup__input_type_description');
 const profileName = document.querySelector('.profile__title');
 const profileJob = document.querySelector('.profile__description');
+const profilePhoto = document.querySelector('.profile__image');
 const addCardForm = document.forms['new-place'];
 const cardName = addCardForm.querySelector('.popup__input_type_card-name');
 const cardImage = addCardForm.querySelector('.popup__input_type_url');
@@ -38,14 +39,70 @@ initialCards.forEach(cardData => {
 
 function handleProfileFormSubmit(evt) {
   evt.preventDefault();
-  profileName.textContent = nameInput.value;
-  profileJob.textContent = jobInput.value;
-  closeModal(editModal);
+  const submitButton = evt.submitter;
+  const originalText = submitButton.textContent;
+  submitButton.textContent = 'Сохранение...';
+  fetch('https://nomoreparties.co/v1/wff-cohort-41/users/me', {
+    method: 'PATCH',
+    headers: {
+      authorization: 'ed2813cd-3e45-42f1-86f8-753c32ce7697',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      name: nameInput.value,
+      about: jobInput.value
+    })
+  })
+    .then(res => {
+      if (!res.ok) {
+        throw new Error('Ошибка при обновлении профиля');
+      }
+      return res.json();
+    })
+    .then((result) => {
+      profileName.textContent = result.name;
+      profileJob.textContent = result.about;
+      closeModal(editModal);
+    })
+    .finally(() => {
+      submitButton.textContent = originalText;
+    });
 }
 
 function handleCardFormSubmit(evt) {
   evt.preventDefault();
-  
+  const submitButton = evt.submitter;
+  const originalText = submitButton.textContent;
+  submitButton.textContent = 'Сохранение...';
+  fetch('https://nomoreparties.co/v1/wff-cohort-41/cards', {
+    method: 'POST',
+    headers: {
+      authorization: 'ed2813cd-3e45-42f1-86f8-753c32ce7697',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      name: cardName.value,
+      link: cardImage.value
+    })
+  })
+    .then(res => {
+      if (!res.ok) {
+        throw new Error('Ошибка при добавлении карточки');
+      }
+      return res.json();
+    })
+    .then((newCard) => {
+        const cardElement = createCard(newCard, openImagePopup);
+        container.prepend(cardElement);
+        cardName.textContent = newCard.name;
+        cardImage.textContent = newCard.link;
+        closeModal(addModal);
+    })
+    .finally(() => {
+      submitButton.textContent = originalText;
+    });
+
+
   const newCardData = {
     name: cardName.value,
     link: cardImage.value
@@ -91,3 +148,56 @@ editForm.addEventListener('submit', handleProfileFormSubmit);
 addCardForm.addEventListener('submit', handleCardFormSubmit);
 
 enableValidation(validationConfig);
+
+
+function loadUserInfo () {
+fetch('https://nomoreparties.co/v1/wff-cohort-41/users/me', {
+  headers: {
+    authorization: 'ed2813cd-3e45-42f1-86f8-753c32ce7697'
+  }
+})
+  .then(res => res.json())
+  .then((result) => {
+    console.log(result);
+    profileName.textContent = result.name;
+    profileJob.textContent = result.about;
+    profilePhoto.value = result.avatar;
+  });
+}
+
+loadUserInfo();
+
+function loadCards() {
+    return Promise.all([
+    fetch('https://nomoreparties.co/v1/wff-cohort-41/cards', {
+      headers: {
+        authorization: 'ed2813cd-3e45-42f1-86f8-753c32ce7697'
+      }
+    }),
+    fetch('https://nomoreparties.co/v1/wff-cohort-41/users/me', {
+      headers: {
+        authorization: 'ed2813cd-3e45-42f1-86f8-753c32ce7697'
+      }
+    })
+  ])
+    .then(([cardsRes, userRes]) => {
+        if (!cardsRes.ok || !userRes.ok) {
+        throw new Error('Один из запросов завершился ошибкой');
+    }
+        return Promise.all([cardsRes.json(), userRes.json()]);
+    })
+    .then(([cards, userData]) => {
+    console.log('Карточки:', cards);
+    console.log('Данные пользователя:', userData);
+     container.innerHTML = '';
+    
+    cards.forEach(cardData => {
+      container.append(createCard(cardData, openImagePopup));
+    });
+    
+    return { cards, userData };
+    })
+}
+
+loadCards();
+
